6e736b56135052c05a0785ee409529424c408714
Dongyu Wang <60108209+dywang0323@users.noreply.github.com>
2023-02-26 21:32:27 -0600

    Add files via upload

---
+++ Scripts/Richness_calculation.R
@@ -0,0 +1,23 @@
+library(vegan)
+library(magrittr)
+library(ggplot2)
+library(tidyverse)
+
+Taxa <- read.csv("/Volumes/Nascell/Figures_soil_incubation/Taxa.csv")
+Taxa %>%
+  group_by(Group) %>%
+  summarize(sobs = specnumber(Value),
+            shannon = diversity(Value, index = "shannon"),
+            simpson = diversity(Value, index = "simpson"),
+            invsimpson = 1/simpson,
+            n=sum(Value)) %>%
+pivot_longer(cols=c(sobs, shannon, invsimpson, simpson),
+            names_to ="metric") %>%
+  ggplot(aes(x=n, y=value)) + 
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~metric, nrow=4, scales="free_y")
+
+
+
+
